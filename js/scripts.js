94739e71178657fa565c3e24d85e50e56b9deb19
henry888wang <65693102+henry888wang@users.noreply.github.com>
2020-06-17 21:44:00 +0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,10 @@ div5.style.display='block';
 div5.style.animation='france 3s  1';
 
 map.style.animation='map 3s 1';
-h1.style.display='none';
-btnx.style.display='none';
+h1.style.animation='abc 2s 1';
+h1.style.opacity='0%';
+btnx.style.animation='abc 2s 1';
+btnx.style.opacity='0%';
 map.style.opacity='100%';
 h2.style.display='block';
 h2.style.animation='h2 linear 2s 3s 1';
